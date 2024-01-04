366c28d01419cb3a8cefa22af73c3bdd3d5e6073
CARTOON <brianngugi224@gmail.com>
2024-01-04 08:47:20 +0300

    Task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
     Calculate the start and end indices for a given page and page size.
 """
 from typing import Tuple
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
